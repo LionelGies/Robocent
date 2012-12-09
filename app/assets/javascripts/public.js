66e2ae4db137213944a8cd2c55dcca9f850c294f
Maruf Hasan <maruf.freelancer@gmail.com>
2012-12-10 01:18:15 +0600

    home page layout

---
+++ app/assets/javascripts/public.js
@@ -0,0 +1,7 @@
+// This is a manifest file that'll be compiled into public.js, which will include all the files
+//= require public_js/jquery.min
+//= require public_js/general
+//= require public_js/jquery.easing.1.3
+//= require public_js/jquery.onebyone
+//= require public_js/jquery.touchwipe.min
+//= require public_js/jquery.jcarousel.min
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,14 +2,14 @@ Robocent::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
-  config.cache_classes = true
+  config.cache_classes = false
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -47,6 +47,9 @@ Robocent::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( public.css )
+  config.assets.precompile += %w( public.js )
+  config.assets.precompile += %w( ie.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
