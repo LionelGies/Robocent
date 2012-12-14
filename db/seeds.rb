0229700a6bf7145dd46ee8fe2fe02d121ac949b8
Maruf Hasan <maruf.freelancer@gmail.com>
2012-12-15 03:34:13 +0600

    select plan graphics

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,14 +13,14 @@ Plan.create([
     maximum_numbers: "500", price_per_call_or_text: "2"},
   {stripe_id: "silver", amount: "900", currency: "usd", interval: "month",
     name: "Silver", trial_period_days: "30", minimum_numbers: "501",
-    maximum_numbers: "999", price_per_call_or_text: "1.9"},
+    maximum_numbers: "1000", price_per_call_or_text: "1.9"},
   {stripe_id: "gold", amount: "1900", currency: "usd", interval: "month",
-    name: "Gold", trial_period_days: "30", minimum_numbers: "1000",
-    maximum_numbers: "9999", price_per_call_or_text: "1.7"},
+    name: "Gold", trial_period_days: "30", minimum_numbers: "1001",
+    maximum_numbers: "10000", price_per_call_or_text: "1.7"},
   {stripe_id: "platinum", amount: "3900", currency: "usd", interval: "month",
-    name: "Platinum", trial_period_days: "30", minimum_numbers: "10000",
-    maximum_numbers: "24999", price_per_call_or_text: "1.5"},
+    name: "Platinum", trial_period_days: "30", minimum_numbers: "10001",
+    maximum_numbers: "24500", price_per_call_or_text: "1.5"},
   {stripe_id: "diamond", amount: "5900", currency: "usd", interval: "month",
-    name: "Diamond", trial_period_days: "30", minimum_numbers: "25000",
-    maximum_numbers: "49999", price_per_call_or_text: "1.3"}
+    name: "Diamond", trial_period_days: "30", minimum_numbers: "24501",
+    maximum_numbers: "50000", price_per_call_or_text: "1.3"}
   ])
