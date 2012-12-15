2a9f4c175a8cc4bcf749b039e16632329b57a191
Maruf Hasan <maruf.freelancer@gmail.com>
2012-12-15 06:29:01 +0600

    fixed confirmation + login

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -8,8 +8,19 @@ class UserSessionsController < ApplicationController
     user = login(params[:email], params[:password], params[:remember_me])
     if user
       if user.activation_state == "active"
-        redirect_back_or_to dashboard_url, :notice => "Logged in!"
+        if user.twilio_phone_number.blank?
+          logout
+          session[:user_temp_id] = user.id
+          redirect_to register_url(:step_id => "2"), :alert => "Please complete your registration to access your dashborad!"
+        elsif user.billing_setting.blank?
+          logout
+          session[:user_temp_id] = user.id
+          redirect_to register_url(:step_id => "3"), :alert => "Please complete your registration to access your dashboard!"
+        else
+          redirect_back_or_to dashboard_url, :notice => "Logged in!"
+        end
       else
+        logout
         flash.now.alert = "Please activate your account!"
         render :new
       end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,18 +69,21 @@ class UsersController < ApplicationController
   def confirmation
     @user = User.find(session[:user_temp_id]) if session[:user_temp_id].present?
     session.delete(:user_temp_id)
+    if @user.present? and @user.activation_state == "active"
+      redirect_to login_path
+    end
   end
 
   def activate
     if @user = User.load_from_activation_token(params[:token])
+      @user.activate!
       if @user.twilio_phone_number.blank?
         session[:user_temp_id] = @user.id
-        redirect_to register_url(:step_id => "2")
+        redirect_to register_url(:step_id => "2"), :alert => "Please complete your registration to access your dashboard!"
       elsif @user.billing_setting.blank?
         session[:user_temp_id] = @user.id
-        redirect_to register_url(:step_id => "3")
+        redirect_to register_url(:step_id => "3"), :alert => "Please complete your registration to access your dashboard!"
       else
-        @user.activate!
         auto_login(@user)
         redirect_back_or_to dashboard_url, :notice => "User was successfully activated."
       end
