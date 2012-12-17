5cb12d97efe016f1a219a8382ecf179d54fe50b4
Maruf Hasan <maruf.freelancer@gmail.com>
2012-12-17 08:14:44 +0600

    fixed billing summary

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,13 +8,7 @@ class DashboardController < ApplicationController
     @call_contacts_count = current_user.lists.sum_number_of_contacts.by_call.first.total.to_i
     @text_contacts_count = current_user.lists.sum_number_of_contacts.by_text.first.total.to_i
     @price_per_call_or_text = current_user.subscription.plan.price_per_call_or_text
-    @cost_for_text = @text_contacts_count.to_f * @price_per_call_or_text
-    @cost_for_call = @call_contacts_count.to_f * @price_per_call_or_text
-    puts @total_contacts_count
-    puts @call_contacts_count
-    puts @text_contacts_count
-    puts @price_per_call_or_text
-    puts @cost_for_text
-    puts @call_for_text
+    @cost_for_text = (@text_contacts_count.to_f * @price_per_call_or_text) / 100
+    @cost_for_call = (@call_contacts_count.to_f * @price_per_call_or_text) / 100
   end
 end
