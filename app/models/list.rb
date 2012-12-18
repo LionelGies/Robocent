9d093117d0f9ba9577880466ff4c430ceb0d42a6
Maruf Hasan <maruf.freelancer@gmail.com>
2012-12-19 02:02:44 +0600

    dependent destroy for list

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,8 +2,8 @@ class List < ActiveRecord::Base
   attr_accessible :keyword, :name, :number_of_contacts, :type_of_list, :user_id
 
   belongs_to  :user
-  has_many    :imports
-  has_many    :contacts
+  has_many    :imports,  :dependent => :destroy
+  has_many    :contacts, :dependent => :destroy
 
   scope :sum_number_of_contacts, :select => "SUM(number_of_contacts) as total"
   scope :by_call, :conditions => "type_of_list = 'call'"
