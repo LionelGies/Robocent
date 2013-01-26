0f07e63641fc94ed806b43869fb378f081718e65
Maruf Hasan <maruf.freelancer@gmail.com>
2013-01-26 06:58:58 +0600

    Fixed image url of promo email

--- app/helpers/mailer_helper.rb
+++ app/helpers/mailer_helper.rb
@@ -1,5 +1,5 @@
 module MailerHelper
   def absolute_image_tag(*args)
-    raw(image_tag(*args).sub /src="(.*?)"/, "src=\"//#{ActionMailer::Base.default_url_options[:protocol]}#{ActionMailer::Base.default_url_options[:host]}" + '\1"')
+    raw(image_tag(*args).sub /src="(.*?)"/, "src=\"http://#{ActionMailer::Base.default_url_options[:host]}" + '\1"')
   end
 end
