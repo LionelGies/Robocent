533d133c61b375f332fd8619b7488e8094aca2f2
Maruf Hasan <maruf.freelancer@gmail.com>
2013-03-12 17:55:29 +0600

    home page and other page design

--- app/assets/javascripts/smarthost/js/custom.js
+++ app/assets/javascripts/smarthost/js/custom.js
@@ -1,55 +1,10 @@
-this.randomtip = function(){
-
-    var pause = 3000; // define the pause for each tip (in milliseconds) Feel free to make the pause longer so users can have time to read the tips :)
-    var length = $("#tips li").length;
-    var temp = -1;
-
-    this.getRan = function(){
-      // get the random number
-      var ran = Math.floor(Math.random()*length) + 1;
-      return ran;
-    };
-    this.show = function(){
-      var ran = getRan();
-      // to avoid repeating tips we need to check
-      while (ran == temp){
-        ran = getRan();
-      };
-      temp = ran;
-      $("#tips li").hide();
-      $("#tips li:nth-child(" + ran + ")").fadeIn("fast");
-    };
-    // initiate the script and also set an interval
-    show(); setInterval(show,pause);
-
-  };
-
-  $(document).ready(function(){
-    randomtip();
-  });
-
-  SyntaxHighlighter.all();
-
-  $(document).ready(function() {
-
-    $(".signin").click(function(e) {
-      e.preventDefault();
-      $("fieldset#signin_menu").toggle();
-      $(".signin").toggleClass("menu-open");
-    });
-
-    $("fieldset#signin_menu").mouseup(function() {
-      return false
-    });
-    $(document).mouseup(function(e) {
-      if($(e.target).parent("a.signin").length==0) {
-        $(".signin").removeClass("menu-open");
-        $("fieldset#signin_menu").hide();
-      }
-    });
-
-  });
-
-  $(function() {
-    $('#forgot_username_link').tipsy({gravity: 'w'});
-  });
+<!-- begin SnapEngage code -->
+(function() {
+    var se = document.createElement('script');
+    se.type = 'text/javascript';
+    se.async = true;
+    se.src = '//commondatastorage.googleapis.com/code.snapengage.com/js/9843552f-3b65-481c-850a-78b1e037bb57.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(se, s);
+})();
+  <!-- end SnapEngage code -->
--- app/assets/javascripts/smarthost/js/scripts.js
+++ app/assets/javascripts/smarthost/js/scripts.js
@@ -1,10 +1,10 @@
 $(function(){
-	$('#slides1').bxSlider({
-		prev_image: '/assets/smarthost/btn_arrow_left.png',
-		next_image: '/assets/smarthost/btn_arrow_right.png',
-		wrapper_class: 'slides1_wrap',
-		margin: 70,
-		auto: false,
-		auto_controls: false
-	});
+//	$('#slides1').bxSlider({
+//		prev_image: '/assets/smarthost/btn_arrow_left.png',
+//		next_image: '/assets/smarthost/btn_arrow_right.png',
+//		wrapper_class: 'slides1_wrap',
+//		margin: 70,
+//		auto: false,
+//		auto_controls: false
+//	});
 });
